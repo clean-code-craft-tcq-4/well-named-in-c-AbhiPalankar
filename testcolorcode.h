c25c10138cdf8757cb5ec7f001a36da463953448
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-06-14 09:38:40 +0530

    Update testcolorcode.h

--- testcolorcode.h
+++ testcolorcode.h
@@ -5,8 +5,8 @@
 #include <assert.h>
 #include "Colorcode.h"
 
-extern void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
-extern void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
+void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
+void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
 
 #endif
 
