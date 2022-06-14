88c0ce378c6ad5be50a7d5234fd980c63e642856
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-06-14 09:57:10 +0530

    Update colorcode.c
    changes added

--- colorcode.c
+++ colorcode.c
@@ -1,19 +1,12 @@
 #include "colorcode.h"
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
+const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 
 void ColorPairToString(const ColorPair* colorPair, char* buffer) {
