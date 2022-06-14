01f2eb7a4613a3a987bca17b1184a7723794e760
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-06-14 11:05:35 +0530

    Update testcolorcode.h
    corrected include name

--- testcolorcode.h
+++ testcolorcode.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <assert.h>
-#include "Colorcode.h"
+#include "colorcode.h"
 
 void testNumberToPair( int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber( enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
